c8e6e5e01afc0cc6755f103790522ed3b7fff099
Duco Lindhout <duco.lindhout@gmail.com>
2021-11-11 15:47:25 +0100

    Finish frontend

--- website/scripts.js
+++ website/scripts.js
@@ -1,28 +1,52 @@
+let protocol = "ws";
+let conn = null;
+
 $(document).ready(function() {
+    connect();
+
     $("#transaction").submit(function(e) {
-        e.preventDefault();
         let data = {
             size: parseInt(parseFloat($("#size").val())),
             price: parseInt(parseFloat($("#price").val())),
             sell: $("#sell:checked").val() === "on" ? true : false,
         }      
-
         data = JSON.stringify(data);
+        e.preventDefault();
 
-        $.ajax({
-            url: 'http://127.0.0.1:8080/',
-            type: 'post',
-            data: data,
-            headers: {"Content-Type": "application/json"},
-            dataType: 'json',
-            success: function (data) {
-                console.info(data);
-            }
-        });
+        if (protocol == "http") {
+    
+            $.ajax({
+                url: 'http://127.0.0.1:8080/',
+                type: 'post',
+                data: data,
+                headers: {"Content-Type": "application/json"},
+                dataType: 'json',
+                success: function (data) {
+                    console.info(data);
+                }
+            });
+        } else {
+            conn.send(data);
+        }
+    })
+
+    $("#ws").click(function() {
+        connect();
+        $(this).css("color", "blue");
+        $("#http").css("color", "black");
+        protocol = "ws"
+    })
+
+    $("#http").click(function() {
+        disconnect();
+        $(this).css("color", "blue");
+        $("#ws").css("color", "black");
+        protocol = "http"
     })
 })
 
 function connect() {
+    disconnect();
     var url = "ws://127.0.0.1:8080/ws/"
 
     conn = new WebSocket(url);
@@ -30,16 +54,20 @@ function connect() {
 
     conn.onopen = function() {
         console.log("Connected");
-
-        setTimeout(function() {
-            conn.send(JSON.stringify({size: 34, price: 3, sell: false}))
-        }, 1000)
     }
     conn.onmessage = function(e) {
         console.info("Received a message ", e.data);
     }
     conn.onclose = function() {
         console.warn("Disconnected from server")
+        conn = null;
+    }
+}
+
+function disconnect() {
+    if (conn != null) {
+        console.log('Disconnecting...');
+        conn.close();
         conn = null;
     }
 }
